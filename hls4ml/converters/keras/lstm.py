4b37f8a8f4f058f9e9330088915cf9d112c36b9c
Nemer Chiedde <chiedde@marsattack2.in2p3.fr>
2021-05-17 13:40:27 +0200

    lstm.py

--- hls4ml/converters/keras/lstm.py
+++ hls4ml/converters/keras/lstm.py
@@ -14,18 +14,21 @@ def parse_lstm_layer(keras_layer, input_names, input_shapes, data_reader, config
     print (keras_layer)
     layer = parse_default_keras_layer(keras_layer, input_names)
 
-    #layer['input_shape'] = keras_layer['config']['batch_input_shape'][1:]
-    layer['input_shape'] = [5,1]
-    #print("timestamps :", keras_layer['config']['batch_input_shape'][1],"inputshape", keras_layer['config']['batch_input_shape'][1:])
-    #layer['n_timestamp'] = keras_layer['config']['batch_input_shape'][1]
-    layer['n_timestamp'] = 5
-    layer['n_in'] = keras_layer['config']['units']
+    print("timestamps :",)
+    if 'batch_input_shape' in keras_layer['config']:
+        layer['input_shape'] = keras_layer['config']['batch_input_shape'][1:]
+        layer['n_timestamp'] = keras_layer['config']['batch_input_shape'][1]
+        layer['n_in'] = keras_layer['config']['units']
+        output_shape = keras_layer['config']['batch_input_shape']
+    else:
+        layer['n_in'] = keras_layer['config']['units']
+        layer['input_shape'] = [layer['n_in'], 1]
+        layer['n_timestamp'] = 5
+        output_shape = [None, 5, layer['n_in']]
+
+
     #if keras_layer['config']['dtype'] == 'int32':
     #    layer['type_name'] = 'integer_input_t'
     #    layer['precision'] = IntegerPrecisionType(width=32)
-    k=[0, None, 5]
-    print("kkkkkkk", k)
-    #print('ERRO :', keras_layer['config']['batch_input_shape'])
-    #output_shape = keras_layer['config']['batch_input_shape']
-    output_shape =  [None, 5, 10]
+
     return layer, output_shape
